a0ea6e5e68a1111e0eacfa66ee03a0ce20eef91c
oki2a24 <oki2a24@oki2a24.com>
2017-08-05 17:05:39 +0900

    Grails と PostgreSQL(phpPGAdmin) のプレイブックを追加する
    - Grails は cen7-grails を使用した
    - PostgreSQL は cen7-postgres9x をベースに、ユーザ、DB追加、設定ファイルを patch に変更した。

---
+++ cen7-grails-postgres/ansible/roles/sdkman/files/sdkman.sh
@@ -0,0 +1,3 @@
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="/usr/local/sdkman"
+[[ -s "/usr/local/sdkman/bin/sdkman-init.sh" ]] && source "/usr/local/sdkman/bin/sdkman-init.sh"
